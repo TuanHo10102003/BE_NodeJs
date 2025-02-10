a72c3d05ccdace2d22b8d5abcdb9935b832010df
TuanHo10102003 <anhtuan6563@gmail.com>
2025-02-10 21:53:06 +0700

    FindProduct

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -31,18 +31,29 @@ module.exports.index = async (req, res) => {
     }
 
     let find = {
-        deleted: false
+        deleted: false,
     }
     
     if(req.query.status){
         find.status = req.query.status;
     }
     
+    let keyword = ""
+
+    if(req.query.keyword){
+        keyword = req.query.keyword
+
+        const regex = new RegExp(keyword, "i");
+        find.title = regex
+    }
+
+
     const products = await Product.find(find);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
+        keyword: keyword
     });
 };
