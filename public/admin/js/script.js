c8404ad091e82495282a2418ab3b4083ec8710b7
TuanHo10102003 <anhtuan6563@gmail.com>
2025-02-15 21:16:13 +0700

    DeleteMulti

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -97,6 +97,16 @@ if (formChangeMulti) {
     const inputsChecked = checkboxMulti.querySelectorAll(
       "input[name='id']:checked"
     );
+
+    const typeChange = e.target.elements.type.value;
+
+    if(typeChange == "delete-all"){
+      const isConfirm = confirm("Bạn có chắc muốn xóa những sản phẩm này")
+      if(!isConfirm){
+        return;
+      }
+    } 
+
     if (inputsChecked.length > 0) {
       let ids = [];
 
