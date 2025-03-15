61bd51a63d3e4b6f394d8affe5c8cd7bc7eaf8ee
TuanHo10102003 <anhtuan6563@gmail.com>
2025-03-15 16:10:50 +0700

    UploadCloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const multer = require("multer")
-const storageMulter = require("../../helpers/storageMulter")
-const upload = multer({ storage: storageMulter() })
+const express = require("express");
+const multer = require("multer");
+
+const upload = multer();
 const router = express.Router();
 
-const controller = require('../../controllers/admin/product.controller');
-const validate = require('../../validates/admin/product.validate')
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
 
-router.get('/', controller.index);
+const controller = require("../../controllers/admin/product.controller");
+const validate = require("../../validates/admin/product.validate");
 
-router.patch('/change-status/:status/:id', controller.changeStatus);
+router.get("/", controller.index);
 
-router.patch('/change-multi', controller.changeMulti);
+router.patch("/change-status/:status/:id", controller.changeStatus);
 
-router.delete('/delete/:id', controller.deleteItem);
+router.patch("/change-multi", controller.changeMulti);
 
-router.get('/create', controller.create);
+router.delete("/delete/:id", controller.deleteItem);
+
+router.get("/create", controller.create);
 
 router.post(
-    '/create',
-    upload.single('thumbnail'),
-    validate.createPost,
-    controller.createPost
+  "/create",
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost
 );
 
-router.get('/edit/:id', controller.edit);
+router.get("/edit/:id", controller.edit);
 
 router.patch(
-    '/edit/:id',
-    upload.single('thumbnail'),
-    validate.createPost,
-    controller.editPatch
+  "/edit/:id",
+  upload.single("thumbnail"),
+  validate.createPost,
+  controller.editPatch
 );
 
-router.get('/detail/:id', controller.detail);
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
