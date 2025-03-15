d9ec667de96983b8fb90de54009b4e1f67ee05b5
TuanHo10102003 <anhtuan6563@gmail.com>
2025-03-16 00:43:54 +0700

    Sort

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -190,7 +190,7 @@ if (uploadImage) {
 
 const sort = document.querySelector("[sort]");
 if (sort) {
-  let url = new URL(window.location.href)
+  let url = new URL(window.location.href);
   const sortSelect = sort.querySelector("[sort-select]");
   const sortClear = sort.querySelector("[sort-clear]");
 
@@ -198,8 +198,23 @@ if (sort) {
     const value = e.target.value;
     const [sortKey, sortValue] = value.split("-");
 
-    url.searchParams.set("sortKey", sortKey)
-    url.searchParams.set("sortValue", sortValue)
+    url.searchParams.set("sortKey", sortKey);
+    url.searchParams.set("sortValue", sortValue);
+    window.location.href = url.href;
+
+  });
+  sortClear.addEventListener("click", () => {
+    url.searchParams.delete("sortKey");
+    url.searchParams.delete("sortValue");
     window.location.href = url.href;
   });
+
+  const sortKey = url.searchParams.get("sortKey");
+  const sortValue = url.searchParams.get("sortValue");
+  if(sortKey && sortValue){
+    const stringSort = `${sortKey}-${sortValue}`
+    const optionSelected = sortSelect.querySelector(`option[value='${stringSort}']`)
+    optionSelected.selected = true
+  }
+
 }
