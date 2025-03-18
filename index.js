e039d13dacad1e46eaf98a463f89dda242ceb6e5
TuanHo10102003 <anhtuan6563@gmail.com>
2025-03-18 20:17:06 +0700

    TinyMCE

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-const express = require('express');
+const express = require("express");
 
-const methodOverride = require("method-override")
+const path = require("path");
 
-const bodyParser = require("body-parser")
+const methodOverride = require("method-override");
 
-const flash = require("express-flash")
+const bodyParser = require("body-parser");
 
-const cookieParser = require("cookie-parser")
+const flash = require("express-flash");
 
-const session = require("express-session")
+const cookieParser = require("cookie-parser");
 
-require('dotenv').config()
+const session = require("express-session");
+
+require("dotenv").config();
 
 const database = require("./config/database");
 
-const systemConfig = require("./config/system")
+const systemConfig = require("./config/system");
 
 database.connect();
 
-const route = require('./routes/client/index.route');
-const routeAdmin = require('./routes/admin/index.route');
-const { default: mongoose } = require('mongoose');
+const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 
 const port = process.env.PORT;
 
-app.use(cookieParser('TUAN10102003'));
-app.use(session({cookie: {maxAge: 60000}}))
+app.use(cookieParser("TUAN10102003"));
+app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
 
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(methodOverride("_method"));
 app.use(express.static(`${__dirname}/public`));
@@ -40,11 +42,16 @@ app.use(express.static(`${__dirname}/public`));
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
+
 //Route
 
 route(app);
 routeAdmin(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
